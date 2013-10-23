0e44aff0f7d6292cac38d11c08d9a47fd8d6dad8
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-10-22 18:49:43 -0700

    Add music_album_url module. It takes in an album name and return results from Metacritic.com/album. Each album in JSON array has a title, a artist name, a release date and a coressponding URL.

---
+++ music_album_url.py
@@ -0,0 +1,29 @@
+__author__ = 'zhoutuoyang'
+import urllib2
+import json
+from bs4 import BeautifulSoup
+
+metacritic = "http://www.metacritic.com"
+base_url = metacritic + "/search/album/{0}/results"
+
+def get_candidate_urls(album_name):
+    # replace all spaces with '+' signs
+    # remove all '/' signs
+    url = base_url.format(album_name.replace(' ', '+').replace('/', ''))
+    print url
+    try:
+        soup = BeautifulSoup(urllib2.urlopen(url))
+    except (urllib2.URLError, urllib2.HTTPError) as e:
+        raise e
+    albums = soup.find_all('li', class_='result')
+    result = []
+    for album in albums:
+        releaseDateTag = album.find('span', class_='data')
+        titleTag = album.find('h3', class_='product_title').a
+        result.append({
+            'title': titleTag.get_text().strip(),
+            'url': metacritic + titleTag.get('href'),
+            'artist': titleTag.get('href').split('/')[-1].replace('-', ' ').title(),
+            'releaseDate': releaseDateTag.get_text().strip()
+        })
+    return json.dumps(result, indent=4)
