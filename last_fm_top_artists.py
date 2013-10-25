4ae5a304dd1951cfd57e221499828515e23b6403
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-10-24 20:31:53 -0700

    Add get_albums and get_album_info

--- last_fm_top_artists.py
+++ last_fm_top_artists.py
@@ -45,3 +45,53 @@ def get_top_artists(country="UNITED STATES", pages=10):
                 'mbid': artist['mbid']
             })
     return artists_container
+
+
+def get_top_albums(mbid):
+    """
+        Get top albums of an artist specifiy by the mbid (musicbrainz ID).
+        The maxium number of albums returned will be less or equal to 10.
+    """
+    limit = 10
+    album_container = []
+    # request Last.fm by using mbid
+    response = requests.get(last_fm_url, params={
+        'api_key': api_key,
+        'method': 'artist.getTopAlbums',
+        'mbid': mbid,
+        'format': 'json',
+        'limit': limit
+    })
+    top_albums = json.loads(response.text)
+    albums = top_albums['topalbums']['album']
+    # generate albums with specific format
+    # each entry with name and mbid
+    for album in albums:
+        album_container.append({
+            'name': album['name'],
+            'mbid': album['mbid']
+        })
+    return albums
+
+
+def get_album_info(mbid):
+    """
+        Get album information bsaed on mbid from Last.fm
+    """
+    # request the album info
+    response = requests.get(last_fm_url, params={
+        'api_key': api_key,
+        'method': 'album.getInfo',
+        'mbid': mbid,
+        'format': 'json',
+    })
+    # load text into dict
+    album = json.loads(response.text)['album']
+    album_trimed = {
+        'name': album['name'],
+        'mbid': album['mbid'],
+        'releaseDate': album['releasedate']
+    }
+    ## load the tracks into album
+    #album_trimed['tracks'] = [];
+    return album_trimed
