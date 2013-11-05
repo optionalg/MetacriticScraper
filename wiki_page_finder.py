6c3ac877a5008468ebf3c5f0907118d4b2147755
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-11-04 21:53:13 -0800

    Add wiki page finder

---
+++ wiki_page_finder.py
@@ -0,0 +1,45 @@
+__author__ = 'zhoutuoyang'
+
+import os
+import json
+import time
+import freebase_service
+
+data_dir = './data'
+wiki_dir = './wiki'
+
+def dirChecking():
+    """
+        Create reviews folder if it does not exist
+    """
+    if not os.path.exists(wiki_dir):
+        os.mkdir(wiki_dir)
+
+
+def scanArtistJSON():
+    """
+        Grab all files end with .json
+        So make sure that every json file in data dir is about an artist
+    """
+    for file in os.listdir(data_dir):
+        if file.endswith('.json'):
+            yield file
+
+
+if __name__ == '__main__':
+    dirChecking()
+    for file in scanArtistJSON():
+        # remove .json
+        artist = file[:-5]
+        url = freebase_service.getArtistWikiLink(artist)
+        # sleep for half a second
+        time.sleep(0.5)
+        if url:
+            try:
+                with open(os.path.join(wiki_dir, artist + '.txt'), mode='w') as f:
+                    f.write(url)
+            except IOError as e:
+                print e.message
+
+
+
