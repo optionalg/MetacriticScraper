2f460766a634f6e9d434647f98dbc984bf1272d5
Zhoutuo Yang <zhoutuoy@gmail.com>
2013-11-13 22:54:24 -0800

    Remove top albums info from Last.fm

--- us_top_artists_builder.py
+++ us_top_artists_builder.py
@@ -2,6 +2,7 @@ from services import last_fm_top_artists
 
 __author__ = 'zhoutuoyang'
 import os
+import re
 import json
 
 """
@@ -24,12 +25,14 @@ def writeArtistJSON(artist):
         print "illegal artist object without a name"
         return
     filename = artist.name + '.json'
+    # escape file name
+    filename = re.sub(r'[/]', ' ', filename)
     try:
         with open(os.path.join(data_dir, filename), 'w+') as f:
             f.write(json.dumps(artist, default=lambda o:o.__dict__, indent=4))
             f.flush()
     except IOError as e:
-        print e.message
+        print e
         print "failed to write file " + filename
 
 
@@ -37,23 +40,6 @@ def buildArtistsJSON():
     # get all the artists
     artists = last_fm_top_artists.get_top_artists("UNITED STATES", 10) # by default it is 10 pages, each with 50 aritists
     for artist in artists:
-        # get top albums
-        top_albums = last_fm_top_artists.get_top_albums(artist)
-        top_albums_enhanced = []
-        # for each album get release dates
-        for album in top_albums:
-            # process album arguments
-            album_info = last_fm_top_artists.get_album_info(album, artist)
-            # if album info is not empty
-            if album_info:
-                top_albums_enhanced.append(album_info)
-            # album info could be empty if something is missing
-            # it is the case for many albums
-            # we append the old album info which misses release date
-            else:
-                top_albums_enhanced.append(album)
-        # assign top album back to the artist
-        artist.topAlbums = top_albums_enhanced
         writeArtistJSON(artist)
 
 
